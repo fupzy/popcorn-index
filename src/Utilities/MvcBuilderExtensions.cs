fbb68e71de2ac92e894d75255e0127bb52005969
Fupzy <charles.dsm@icloud.com>
2026-03-15 10:55:30 +0100

    remove commented code

--- src/Utilities/MvcBuilderExtensions.cs
+++ src/Utilities/MvcBuilderExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,23 +10,12 @@ public static class MvcBuilderExtensions
     /// Scans all referenced assemblies in the application and adds any assembly containing
     /// a <see cref="ControllerBase"/> implementation as an <see cref="ApplicationPart"/> to the MVC builder.
     /// </summary>
-    //public static IMvcBuilder AddParts(this IMvcBuilder mvcBuilder)
-    //{
-    //    foreach (var assembly in Helpers.GetAllReferencedAssembliesWithTypeAssignableTo<ControllerBase>())
-    //    {
-    //        mvcBuilder.AddApplicationPart(assembly);
-    //    }
-
-    //    return mvcBuilder;
-    //}
-
     public static IMvcBuilder AddParts(this IMvcBuilder mvcBuilder)
     {
         var assemblies = Helpers.GetAllReferencedAssembliesWithTypeAssignableTo<ControllerBase>();
 
         foreach (var assembly in assemblies)
         {
-            Console.WriteLine($"[MvcBuilderExtensions] Adding ApplicationPart from assembly: {assembly.FullName}");
             mvcBuilder.AddApplicationPart(assembly);
         }
 
