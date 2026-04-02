e503ba6195ea7e234e935133f89ead7c1b23c7f5
Charles <119613962+fupzy@users.noreply.github.com>
2026-04-02 22:21:31 +0200

    Feature : add application styles (#6)

--- ui/src/app/app.ts
+++ ui/src/app/app.ts
@@ -1,11 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html'
+  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardContent, RouterOutlet],
+  templateUrl: './app.html',
+  host: {
+    class: 'h-screen w-full flex flex-1'
+  }
 })
-export class App {
-  protected readonly title = signal('popcorn-index');
-}
+export class App {}
