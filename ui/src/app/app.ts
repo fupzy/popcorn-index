5db2b479af31c3e677b0accb0860df36ee208196
Fupzy <charles.dsm@icloud.com>
2026-04-02 22:15:12 +0200

    add base app component and meterial palette

--- ui/src/app/app.ts
+++ ui/src/app/app.ts
@@ -1,11 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html'
+  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardContent, RouterOutlet],
+  templateUrl: './app.html',
+  host: {
+    class: 'h-screen w-full flex flex-1'
+  }
 })
-export class App {
-  protected readonly title = signal('popcorn-index');
-}
+export class App {}
