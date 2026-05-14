f1ff3a2d5af042bb5f39448e052ff6ef52fe7a79
Fupzy <charles.dsm@icloud.com>
2026-05-14 18:57:38 +0200

    import ordering

--- ui/src/app/shared/loading-shell/loading-shell.spec.ts
+++ ui/src/app/shared/loading-shell/loading-shell.spec.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+
 import { MaterialTesting } from '@testing';
 
 import { LoadingShell } from './loading-shell';
