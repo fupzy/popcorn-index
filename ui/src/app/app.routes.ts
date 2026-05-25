069551d3197ae0692313029528106fefbef49fed
Charles <119613962+fupzy@users.noreply.github.com>
2026-05-25 18:18:04 +0200

    Tech : add lazy loading (#42)

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,23 +1,34 @@
 import { Routes } from '@angular/router';
 
-import { Home } from './home/home';
-import { NotFound } from './not-found/not-found';
-import { Login } from './authentication/login/login';
-import { Register } from './authentication/register/register';
-import { MovieDetail } from './media-detail/movie-detail/movie-detail';
-import { SeriesDetail } from './media-detail/series-detail/series-detail';
-import { Search } from './search/search/search';
-import { MyReviews } from './my-reviews/my-reviews';
 import { authenticatedGuard } from './authentication/authenticated.guard';
 
 export const routes: Routes = [
-  { path: 'home', component: Home },
-  { path: 'search', component: Search },
-  { path: 'movie-detail/:id', component: MovieDetail },
-  { path: 'series-detail/:id', component: SeriesDetail },
-  { path: 'my-reviews', component: MyReviews, canActivate: [authenticatedGuard] },
-  { path: 'login', component: Login },
-  { path: 'register', component: Register },
+  { path: 'home', loadComponent: () => import('./home/home').then((m) => m.Home) },
+  { path: 'search', loadComponent: () => import('./search/search/search').then((m) => m.Search) },
+  {
+    path: 'movie-detail/:id',
+    loadComponent: () => import('./media-detail/movie-detail/movie-detail').then((m) => m.MovieDetail)
+  },
+  {
+    path: 'series-detail/:id',
+    loadComponent: () => import('./media-detail/series-detail/series-detail').then((m) => m.SeriesDetail)
+  },
+  {
+    path: 'my-reviews',
+    loadComponent: () => import('./my-reviews/my-reviews').then((m) => m.MyReviews),
+    canActivate: [authenticatedGuard]
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./authentication/login/login').then((m) => m.Login)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./authentication/register/register').then((m) => m.Register)
+  },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', component: NotFound }
+  {
+    path: '**',
+    loadComponent: () => import('./not-found/not-found').then((m) => m.NotFound)
+  }
 ];
